102f4fa7237679cc6d00610ff1a9b7c3cd92547f
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-03-19 04:26:57 -0700

    test

--- models/city.py
+++ models/city.py
@@ -10,5 +10,8 @@ from models.place import Place
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
