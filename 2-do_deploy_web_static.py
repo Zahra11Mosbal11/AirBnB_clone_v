466d95185b43e54de50a306d095973cc1465431d
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-04-04 10:44:37 -0700

    plz

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Deploy archive!"""
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['34.207.58.214', '3.94.213.47']
+
+
+def do_deploy(archive_path):
+    """distributes an archive to your web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
